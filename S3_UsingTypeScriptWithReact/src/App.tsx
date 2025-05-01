f54a25546ae908c0454c8d50ae15dac935abf9f2
hhoang1204 <hhoang12042003@gmail.com>
2025-05-01 11:33:13 +0700

    s4 advanced component types

---
+++ S3_UsingTypeScriptWithReact/src/App.tsx
@@ -0,0 +1,41 @@
+import goalsImg from "./assets/goals.jpg";
+import CourseGoalList from "./components/CourseGoalList";
+import Header from "./components/Header";
+import { useState } from "react";
+import NewGoal from "./components/NewGoal";
+
+export type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
+export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal(title: string, description: string) {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title,
+        description,
+      };
+
+      return [...prevGoals, newGoal];
+    });
+  }
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== id));
+  }
+
+  return (
+    <main>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <NewGoal onAddGoal={handleAddGoal} />
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
+    </main>
+  );
+}
---
+++ S3_UsingTypeScriptWithReact/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { type ReactNode } from "react";
+
+type HeaderProps = {
+  children: ReactNode;
+  image: { src: string; alt: string };
+};
+
+export default function Header({ children, image }: HeaderProps) {
+  return (
+    <header>
+      <img {...image} />
+      {children}
+    </header>
+  );
+}
